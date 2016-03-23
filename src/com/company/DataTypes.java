4f42a682c4253a6612360d07e4a0c13d20426e34
Nicky Marino <nsmarino96@gmail.com>
2016-03-22 23:50:02 -0400

    Types of data

---
+++ src/com/company/DataTypes.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int chickens = 3;
+        double bacon = 5.5;
+        String tractor = "There is none!";
+
+        System.out.println("Chickens:\n" + chickens);
+        System.out.println("Bacon (kg):\n" + bacon);
+        System.out.println("A tractor:\n" + tractor);
+        System.out.println("\nIn a nutshell:\n" + chickens);
+        System.out.println(bacon + "\n" + tractor);
+    }
+}
