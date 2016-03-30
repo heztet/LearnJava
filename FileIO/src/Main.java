9911f7adbe32a3abc2610a6db415047c25d24eba
Nicky Marino <nsmarino96@gmail.com>
2016-03-29 22:42:27 -0400

    File write

--- FileIO/src/Main.java
+++ FileIO/src/Main.java
@@ -1,6 +1,37 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        File file = new File("test.txt");
+
+        try {
+            file.createNewFile();
+        } catch (Exception e) {
+            System.out.println("File could not be written. Error: " + e.getMessage());
+        }
+
+    }
+
+    // Returns file contents (with newline chars)
+    public String readFile(File f) throws Exception{
+        Scanner reader = null;
+        String string = "";
+
+        /* try {
+            reader = new Scanner(f);
+        } catch (Exception e) {
+            System.out.println("We couldn't find the file. Error: " + e.getMessage());
+            return;
+        } */
+
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine();
+            string += line + "\n";
+        }
+
+        reader.close();
+        return string;
     }
 }
