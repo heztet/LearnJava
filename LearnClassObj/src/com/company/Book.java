542f0de75d106a1e53084b2d4840796198bc49e7
Nicky Marino <nsmarino96@gmail.com>
2016-03-29 20:34:43 -0400

    Add hash methods to Book

--- LearnClassObj/src/com/company/Book.java
+++ LearnClassObj/src/com/company/Book.java
@@ -35,8 +35,43 @@ public class Book {
         this.contents = contents;
     }
 
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+
+        if (getClass() != object.getClass()) {
+            return false;
+        }
+
+        // Type casting
+        // (object has class Book)
+        Book compared = (Book) object;
+        // Publishing years must be the same
+        if (this.publishingYear != compared.getPublishingYear()) {
+            return false;
+        }
+        // Name can't be null and must be equal
+        if (this.name == null || !this.name.equals(compared.getName())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
     public String toString() {
         return "Name: " + this.name + " (" + this.publishingYear + ")\n"
                 + "Contents: " + this.contents;
+    }
+
+    // Give both properties of Book a hashCode for HashMap to work properly
+    public int hashCode() {
+        if (this.name == null) {
+            return 7;
+        }
+
+        return this.publishingYear + this.name.hashCode();
     }
 }
--- LearnClassObj/src/com/company/BookMain.java
+++ LearnClassObj/src/com/company/BookMain.java
@@ -4,11 +4,24 @@ import java.util.HashMap;
 
 public class BookMain {
     public static void main(String[] args) {
+        // Hash map examples
         Book sense = new Book("Sense and Sensibility", 1811, "...");
         Book pride = new Book("Pride and Prejudice", 1813, "...");
 
         HashMap<String, Book> bookCollection = new HashMap<String, Book>();
         bookCollection.put(sense.getName(), sense);
         bookCollection.put(pride.getName(), pride);
+
+        // Equals method example
+        Book book1 = new Book("Book", 2000, "...");
+        Book book2 = new Book("Book", 2000, "...");
+
+        /*
+        if (book1.equals(book2)) {
+            System.out.println("The books are the same");
+        } else {
+            System.out.println("The books are not equal");
+        }
+        */
     }
 }
